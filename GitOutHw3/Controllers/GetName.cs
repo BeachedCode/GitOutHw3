35eccd3572d6c59f680c2972407894ff63f3801d
Ethan Beach <Beached@mail.uc.edu>
2023-10-31 21:26:47 -0400

    Change

---
+++ GitOutHw3/Controllers/GetName.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOutHw3.Controllers
+{
+    public class GetName : Controller
+    {
+        public IActionResult GetMyName()
+        {
+            var myName = new { Name = "Your Name" };
+            return Json(myName);
+        }
+    }
+}
